a87d5c164eb93a4b0a472b09573e97e916551f8f
Hafizh Ibrahim <hafizhibra42@gmail.com>
2021-08-17 21:09:58 +0700

    Initial Commit

---
+++ GameCatalogueWithDetail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameCatalogueWithDetail
+//
+//  Created by Hafizh Ibrahim on 17/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
